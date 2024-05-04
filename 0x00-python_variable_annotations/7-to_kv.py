f75fb9754b1a44b5fb6af36239d0754571b6afaa
LancelotSam <skmburu110@gmail.com>
2024-05-03 17:20:13 -0700

    python var annotations

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """ Type-annotated function to_kv that takes
+        strings, integers and floats arguments.
+        Args:
+            k: string type.
+            v: int or float type.
+        Return:
+            Tuple.
+    """
+    return (k, pow(v, 2))
